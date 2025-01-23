27d81040ba76189da5135f8d22ac5d893e0c2b75
Tahsin Efe Yılmaz <thsnfylmz2002@gmail.com>
2025-01-23 03:07:57 +0300

    Basic Car Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f; 
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        // move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
